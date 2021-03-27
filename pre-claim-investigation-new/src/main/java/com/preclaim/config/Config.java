52a445145bbe0712745fe5cdd31c60bb6f5a9202
s.dixon <s.dixon@xangars.com>
2021-03-27 11:57:05 +0530

    Single case changes

--- pre-claim-investigation-new/src/main/java/com/preclaim/config/Config.java
+++ pre-claim-investigation-new/src/main/java/com/preclaim/config/Config.java
@@ -5,7 +5,7 @@ import java.io.File;
 public interface Config {
 
 	String site_name = "Pre-Claim Investigation";
-	String version = "Live version 2.1";
+	String version = "UAT version 0.4";
 	String upload_directory = "C:\\Pre-Claim Investigation\\uploads" + File.separator;
 	String upload_url = "http://13.235.150.162/Claims/";
 }
