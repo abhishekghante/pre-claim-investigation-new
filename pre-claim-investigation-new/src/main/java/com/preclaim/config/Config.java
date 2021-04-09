5de80a3cde365f9ad2b7a67f375f9bfcfed3a7ea
ghant <ghant@192.168.1.209>
2021-04-09 20:37:06 +0530

    Mail document attached

--- pre-claim-investigation-new/src/main/java/com/preclaim/config/Config.java
+++ pre-claim-investigation-new/src/main/java/com/preclaim/config/Config.java
@@ -8,4 +8,5 @@ public interface Config {
 	String version = "UAT version 0.4";
 	String upload_directory = "C:\\Pre-Claim Investigation\\uploads" + File.separator;
 	String upload_url = "http://13.235.150.162/Claims/";
+	String Path ="C:\\Pre-Claim Investigation\\uploads\\";
 }
